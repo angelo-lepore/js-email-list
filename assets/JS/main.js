e5ac3854143bc5fcec9eeb15a7c6c05d5ff58872
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-16 17:06:58 +0200

    exercise-finished-and-bonus-1

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,5 +1,18 @@
-fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+const emailListEl = document.getElementById("emailList");
+// const btnGeneraEl = document.getElementById("btnGenera");
+
+const array = Array.from({ length: 10 });
+
+array.forEach(() => {
+  fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((response) => response.json())
+    .then((data) => {
+      const mailRandom = data.response;
+
+      // gen <li>
+      const li = document.createElement("li");
+      li.textContent = mailRandom;
+      li.classList.add("list-group-item");
+      emailListEl.appendChild(li);
+    });
+});
